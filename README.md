5214ee56d9835842902dad5643f5908b3460b3b7
GrannyOlli <o.mahnke@web.de>
2019-03-12 10:42:09 +0100

    Kommunikation Webapp mit Apiserver

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ mvn clean compile spring-boot:run
 
 ## Image bauen und pushen 
 docker login -u developer -p `oc whoami -t` docker-registry-default.192.168.42.112.nip.io
-mvn clean install -Ddocker
+mvn clean process-resources install -Ddocker
 
 ## Anwendung testen
 API-Endpunkt Swagger: swagger-ui.html
+Webapp: http://localhost:8081/app
--- webapp/src/main/java/de/bdr/labdays/frontend/FrontendController.java
+++ webapp/src/main/java/de/bdr/labdays/frontend/FrontendController.java
@@ -1,13 +1,42 @@
 package de.bdr.labdays.frontend;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.ui.Model;
 
 @Controller
 public class FrontendController {
 
+	public static final class Response {
+		private String content;
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+	}
+
+	@Value("${apiserver.url}")
+	private String apiserverUrl;
+
+	private RestTemplate restTemplate;
+
+	public FrontendController(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
+
 	@GetMapping("/app")
-	public String index() {
+	public String index(Model model) {
+		Response response = this.restTemplate.getForObject(apiserverUrl + "/greeting?name={value}", Response.class,
+				"foo");
+		model.addAttribute("value", response.getContent());
 		return "index";
 	}
 
